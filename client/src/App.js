9aad2cee9f0e15df70ee7d6faf5618822627c27a
didemertens <d.g.j.mertens@gmail.com>
2020-05-17 16:22:24 +0100

    add normalize css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
+import 'normalize.css'
+import './styles/main.scss'
+
 import BookList from './components/BookList/BookList'
 
 const App = () => {
   return (
     <div data-test="app-component">
       <h1>Judge a book by its cover</h1>
+      <h2>hello</h2>
       <BookList />
     </div>
   )
