ed3019d4fab75fef4a426604719e717526cec6fc
didemertens <d.g.j.mertens@gmail.com>
2020-05-20 10:01:36 +0100

    add light/dark mode with water.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import 'normalize.css'
 import './styles/main.scss'
@@ -6,12 +6,42 @@ import './styles/main.scss'
 import BookList from './components/BookList/BookList'
 
 const App = () => {
+  const [theme, setTheme] = useState('light')
+  const style = document.getElementById('style-direction')
+
   return (
     <div data-test="app-component">
       <h1>Judge a book by its cover</h1>
+      <button
+        onClick={() => {
+          if (theme === 'light') {
+            style.href = 'https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css'
+            setTheme('dark')
+          } else {
+            style.href = 'https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css'
+            setTheme('light')
+          }
+        }}
+      >
+        {
+          theme === 'light'
+            ?
+            '🌙'
+            :
+            '☀️'
+        }
+      </button>
       <BookList />
     </div>
   )
 }
 
-export default App;
+export default App
+
+
+
+// if (lang === 'ar') {
+//   style.href = '/css/style-ltr.css';
+// } else {
+//   style.href = '/css/style-rtl.css';
+// }
