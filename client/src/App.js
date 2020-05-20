9465effa68aa170238eb69bd5515d2f29096e49e
didemertens <d.g.j.mertens@gmail.com>
2020-05-20 10:07:03 +0100

    make light theme default

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,23 @@ import BookList from './components/BookList/BookList'
 
 const App = () => {
   const [theme, setTheme] = useState('light')
-  const style = document.getElementById('style-direction')
+  const styleCSS = document.getElementById('style-direction')
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      styleCSS.href = 'https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css'
+      setTheme('dark')
+    } else {
+      styleCSS.href = 'https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css'
+      setTheme('light')
+    }
+  }
 
   return (
     <div data-test="app-component">
       <h1>Judge a book by its cover</h1>
       <button
-        onClick={() => {
-          if (theme === 'light') {
-            style.href = 'https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css'
-            setTheme('dark')
-          } else {
-            style.href = 'https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/dark.min.css'
-            setTheme('light')
-          }
-        }}
+        onClick={() => toggleTheme()}
       >
         {
           theme === 'light'
